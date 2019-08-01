320f710dcc54c346f200257af1ebce026857aa4b
duper3 <53580068+duper3@users.noreply.github.com>
2019-08-01 20:32:43 +0100

    Update quit.cs

--- quit.cs
+++ quit.cs
@@ -1,21 +1,30 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+
 public class quit : MonoBehaviour
 {
-
     public Button ButtenQuit;
+    public Button Restart;
+    [SerializeField] Text textComponent;
+    [SerializeField] State gameStart;
+
+    State state;
+
+    public object bank { get; private set; }
+
     void Start()
     {
-     ButtenQuit.onClick.AddListener(() => buttenquit());
-     
+        ButtenQuit.onClick.AddListener(() => buttenquit());
+        Restart.onClick.AddListener(() => restart());
     }
 
     void Update()
     {
         KeyQuit();
+        //restart();
     }
 
     private void buttenquit()
@@ -23,6 +32,7 @@ public class quit : MonoBehaviour
         Application.Quit();
         //Debug.Log("press butten quit");
     }
+
     public void KeyQuit()
     {
         if (Input.GetKeyDown(KeyCode.Escape))
@@ -31,5 +41,45 @@ public class quit : MonoBehaviour
             //Debug.Log("Escape press game closing");
         }
     }
+
+    public void restart()
+    {
+        Debug.Log("restart button press");
+        
+        
+    }
+
+    /*
+     * buttenquit(State state)
+    {
+           private void restart(State state)
+       {
+               if (Input.GetButtonDown(string Restart))
+               {
+                   Debug.Log("restart button press");
+
+                   state = gameStart;
+                   textComponent.text = state.GetStateStory();
+
+               }
+
+
+                       Debug.Log("restart button press");
+
+                       state = gameStart;
+                       textComponent.text = state.GetStateStory();
+
+                       Debug.Log("get GetNextStates ");
+
+                       var nextStates = state.GetNextStates();
+                       state = nextStates[0];
+                       textComponent.text = state.GetStateStory();
+                       Debug.Log("get nextStates " + nextStates);
+
+           }
+            */
+
+
+
 }
 
